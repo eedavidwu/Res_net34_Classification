0ef9293b73a25a41007b81dc967898c7ee2c4c2f
eedavidwu <43716059+eedavidwu@users.noreply.github.com>
2019-08-01 10:59:51 +0800

    Delete TestParams.py

--- TestParams.py
+++
@@ -1,7 +0,0 @@
-class TestParams(object):
-    def __init__(self):
-        self.gpus = None  # default to use CPU mode
-        self.ckpt = None                 # path to the pretrained ckpt file
-        self.testdata_dir = None  # default `save_dir`
-
-params = TestParams()
